ce9a5b0f30ca2c5c16e51b85fd4e4158c772c81e
Albedo <raincadence27@gmail.com>
2022-03-23 00:24:46 -0700

    create tables

---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE orgHub_db;
+
+USE orgHub_db;
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
