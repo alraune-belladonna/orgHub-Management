8e57f076e87371b7139efd848c3aaff1bb007578
Albedo <raincadence27@gmail.com>
2022-04-24 17:30:04 -0700

    add department

--- server.js
+++ server.js
@@ -8,3 +8,20 @@ const db = mysql.createConnection({
 },
 console.log('connected to orgHub_db')
 )
+
+const addDepartment = () => {
+  inquirer.prompt([
+    {
+      name: `departmentName`,
+      message: `Name of new department?`,
+      type: `input`,
+    }])
+    .then(newDepartment => {
+      db.query("INSERT INTO departments SET ?", newDepartment, err => {
+        if (err) {
+          console.log(err)
+        }
+        console.log("department added")
+      })
+    })
+}
